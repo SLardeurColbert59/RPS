4672dba591bddf352a94c6b6dbfb198adec07ea7
Lardeur Stevan <stevan.lardeur1@gmail.com>
2022-03-04 09:00:35 +0100

    Maj 5

---
+++ router.py
@@ -0,0 +1,16 @@
+class Router:
+    
+    NUM = 0
+    def __init__(self, L):
+        self.NUM += 1
+        self.name="Rt"+str(self.NUM)
+        pass
+    
+    def addInterface(self, ip):
+        pass
+        
+    def getIpByInterface(self, name):
+        pass
+    
+    def removeInterface(self, ip):
+        pass
